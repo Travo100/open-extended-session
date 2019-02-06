87f58bad29f187f265c62dc780630d911c972fc8
Travo100 <travo100@gmail.com>
2019-02-05 17:38:31 -0800

    Added .env and ready to deploy

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import withAuth from './withAuth';
 import API from '../utils/API';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#yourAppElement')
 
 class Profile extends Component {
 
-  state = {
-    username: "",
-    email: ""
-  };
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      email: "",
+      modalIsOpen: false,
+      name: ""
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  
 
   componentDidMount() {
     API.getUser(this.props.user.id).then(res => {
@@ -19,13 +45,58 @@ class Profile extends Component {
     });
   }
 
+  goToSignup = e => {
+    this.props.history.replace("/signup");
+  }
+
+  inputChange = e => {
+    const {name, value} = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   render() {
     return (
       <div className="container Profile">
-        <h1>On the profile page!</h1>
+        <h1>On the profile page! {this.state.name}</h1>
         <p>Username: {this.state.username}</p>
         <p>Email: {this.state.email}</p>
         <Link to="/">Go home</Link>
+        <button onClick={this.goToSignup}>Sign up!</button>
+        <button onClick={this.openModal}>Open Modal</button>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input type="text" name="name" onChange={this.inputChange} />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
       </div>
     )
   }
